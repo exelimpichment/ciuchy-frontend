26fbae52fbb6aad122f254f556cb5fe78bf48e7f
exelimpichment <oldestspy@gmail.com>
2023-04-10 02:52:27 +0200

    serverless tag added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'out',
+  target: 'serverless',
   compiler: {
     styledComponents: true,
   },
